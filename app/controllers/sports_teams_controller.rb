2a031448e27e78c9b94e3ac36c16c9a790ca2b21
Sam Evans <evansst@icloud.com>
2020-07-14 15:34:32 -0600

    Adds find by name route to sports teams.

--- app/controllers/sports_teams_controller.rb
+++ app/controllers/sports_teams_controller.rb
@@ -1,11 +1,15 @@
 class SportsTeamsController < ApplicationController
   def index
-    sports_teams = SportsTeam.all
-    render json: sports_teams
+    render json: SportsTeam.all
   end
 
   def show
     sports_team = SportsTeam.find_by(id: params[:id])
+    render json: sports_team
+  end
+  
+  def show_by_name
+    sports_team = SportsTeam.find_by(name: params[:name])
     render json: sports_team
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :fantasy_leagues
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/sports_teams/find_by/:name', to: 'sports_teams#show_by_name'
 end
